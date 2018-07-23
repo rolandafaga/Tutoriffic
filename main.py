202bef94b276c7da5c3fb4f08455fa40fc2e4a7d
Cara Harben <cara.harben@gmail.com>
2018-07-23 09:57:33 -0700

    renamed css file

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import webapp2
 import jinja2
 import os
 from google.appengine.ext import ndb
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+
+class LoginHandler(webapp2.RequestHandler):
+    
