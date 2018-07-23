69fc22bd994878f44d9c0e28e9871097ad7bb2b3
Cara Harben <cara.harben@gmail.com>
2018-07-23 10:07:52 -0700

    added handler bases

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
 )
 
-class LoginHandler(webapp2.RequestHandler):
-    
+class HomeHandler(webapp2.RequestHandler):
+    def get(self):
+        home_template = jinja_enc.get_template('templates/homepage.html')
+        self.response.write(home_template_template.render())
+
+class ProfileHandler(webapp2.RequestHandler):
+    def get(self):
+        create_template = jinja_env.get_template('templates/create.html')
+        self.response.write(create_template.render())
+
+
+app = webapp2.WSGIApplication([
+    ('/', )
+])
